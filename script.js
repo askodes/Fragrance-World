f8997130f663f639ccf45324dc6486087f6fcfe5
askodes <seaarts14@gmail.com>
2026-03-17 18:02:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("mouseover", () => {
+link.style.opacity = "0.7";
+});
+
+link.addEventListener("mouseout", () => {
+link.style.opacity = "1";
+});
+
+});
